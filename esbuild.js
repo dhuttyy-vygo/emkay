5524f97107badb35daaf2926a36a5aa99c252645
dhuttyy-vygo <45688413+dhuttyy-vygo@users.noreply.github.com>
2025-02-12 11:16:08 +0000

    esbuild for prod

--- esbuild.js
+++ esbuild.js
@@ -4,7 +4,7 @@ esbuild.build({
   entryPoints: ['./src/app.js'],
   bundle: true, // Bundles all dependencies into a single file
   minify: true, // Minifies the output
-  outfile: './dist/bundled-app.js', // Single output file
+  outfile: './dist/bundled-app-min.js', // Single output file
   platform: 'browser', // Target browser environments
   sourcemap: false, // Optional: Set to 'inline' or 'external' if needed
   target: ['es6'], // Target modern browsers
