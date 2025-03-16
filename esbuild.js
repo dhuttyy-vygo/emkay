7be84b8dfdc440ef0f72b824fb0751f0d8bb296b
dhuttyy-vygo <45688413+dhuttyy-vygo@users.noreply.github.com>
2025-03-16 17:36:42 +0000

    clean up

--- esbuild.js
+++ esbuild.js
@@ -1,10 +1,22 @@
 const esbuild = require('esbuild');
 
+// Build configuration for app.js
 esbuild.build({
   entryPoints: ['./src/app.js'],
   bundle: true, // Bundles all dependencies into a single file
   minify: true, // Minifies the output
   outfile: './dist/bundled-app-min.js', // Single output file
+  platform: 'browser', // Target browser environments
+  sourcemap: false, // Optional: Set to 'inline' or 'external' if needed
+  target: ['es6'], // Target modern browsers
+}).catch(() => process.exit(1));
+
+// Build configuration for swiperapp.js
+esbuild.build({
+  entryPoints: ['./src/swiperapp.js'],
+  bundle: true, // Bundles all dependencies into a single file
+  minify: true, // Minifies the output
+  outfile: './dist/bundled-swiper-min.js', // Single output file
   platform: 'browser', // Target browser environments
   sourcemap: false, // Optional: Set to 'inline' or 'external' if needed
   target: ['es6'], // Target modern browsers
